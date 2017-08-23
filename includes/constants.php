e7c579f1294f940f23a7dbee20b221fa6e29a407
mechanicalheart <randommechanicalheart@gmail.com>
2017-08-23 12:52:35 +0600

    adminka

--- includes/constants.php
+++ includes/constants.php
@@ -1,8 +1,8 @@
 <?php
 // Константы базы данных
 define("DB_SERVER", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_USER", "adminka");
+define("DB_PASS", "admin12345");
 define("DB_NAME", "userlistdb");
 
 ?>
