ad1ab6d8a40f84293954aa101c330275921437f4
mechanicalheart <randommechanicalheart@gmail.com>
2017-08-23 12:10:08 +0600

    php fix

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 session_start();
 
 if(!isset($_SESSION["session_username"])):
-header("location:login.php");
+  header("location:register.php");
 else:
 ?>
 <?php include("includes/header.php"); ?>
