d99dea74ffb4105f217fdc3d58f815598bd9fce6
mechanicalheart <randommechanicalheart@gmail.com>
2017-08-23 12:44:21 +0600

    ggg

--- includes/constants.php
+++ includes/constants.php
@@ -1,6 +1,6 @@
 <?php
 // Константы базы данных
-define("DB_SERVER", "45.55.49.130");
+define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "userlistdb");
